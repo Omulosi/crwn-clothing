d0de3e4962bfb39d58491d539d0836af92295ab0
Omulosi <mulongojohnpaul@gmail.com>
2021-08-15 18:15:37 +0300

    Add Sign in page with custom input and button components and a route to the  sign in page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import CustomButton from "../custom-button/custom-button.component";
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in.styles.scss";
+
+export default class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            value={this.state.email}
+            required
+            handleChange={this.handleChange}
+            label="Email"
+          />
+
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+            label="Password"
+          />
+          <CustomButton type="submit">Sign In</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
